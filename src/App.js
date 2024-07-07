73c5c7bb77049f765475cf28f10daa4139e870b6
ptthao0801 <ptthao0801@gmail.com>
2024-07-07 17:05:45 +0700

    first cm

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import MyClock from "./components/MyClock";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div class="Container">
+            <MyClock />
+        </div>
+    );
 }
 
 export default App;
